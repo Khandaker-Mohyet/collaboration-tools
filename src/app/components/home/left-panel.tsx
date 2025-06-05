c871b716fd8aab46bbc6a0b2254a8c433f0cc4f4
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-06 00:57:41 +0600

    get dummi data

--- src/app/components/home/left-panel.tsx
+++ src/app/components/home/left-panel.tsx
@@ -1,8 +1,54 @@
-<main className='m-5'>
-	<div className='flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-panel'>
-		{/* Green background decorator for Light Mode */}
-		<div className='fixed top-0 left-0 w-full h-36 bg-green-primary dark:bg-transparent -z-30' />
-		<LeftPanel />
-		<RightPanel />
-	</div>
-</main>
+import { ListFilter, LogOut, MessageSquareDiff, Search, User } from "lucide-react";
+import { Input } from "../ui/input";
+import ThemeSwitch from "./theme-switch";
+
+const LeftPanel = () => {
+	const conversations = [];
+
+	return (
+		<div className='w-1/4 border-gray-600 border-r'>
+			<div className='sticky top-0 bg-left-panel z-10'>
+				{/* Header */}
+				<div className='flex justify-between bg-gray-primary p-3 items-center'>
+					<User size={24} />
+
+					<div className='flex items-center gap-3'>
+						<MessageSquareDiff size={20} /> {/* TODO: This line will be replaced with <UserListDialog /> */}
+						<ThemeSwitch />
+						<LogOut size={20} className='cursor-pointer' />
+					</div>
+				</div>
+				<div className='p-3 flex items-center'>
+					{/* Search */}
+					<div className='relative h-10 mx-3 flex-1'>
+						<Search
+							className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10'
+							size={18}
+						/>
+						<Input
+							type='text'
+							placeholder='Search or start a new chat'
+							className='pl-10 py-2 text-sm w-full rounded shadow-sm bg-gray-primary focus-visible:ring-transparent'
+						/>
+					</div>
+					<ListFilter className='cursor-pointer' />
+				</div>
+			</div>
+
+			{/* Chat List */}
+			<div className='my-3 flex flex-col gap-0 max-h-[80%] overflow-auto'>
+				{/* Conversations will go here*/}
+
+				{conversations?.length === 0 && (
+					<>
+						<p className='text-center text-gray-500 text-sm mt-3'>No conversations yet</p>
+						<p className='text-center text-gray-500 text-sm mt-3 '>
+							We understand {"you're"} an introvert, but {"you've"} got to start somewhere 😊
+						</p>
+					</>
+				)}
+			</div>
+		</div>
+	);
+};
+export default LeftPanel;
