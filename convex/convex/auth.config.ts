65e7f9c287a3a8f63fcbbbba186a92d8066d67ff
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-08 18:13:15 +0600

    auth connected

---
+++ convex/convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig =  {
+  providers: [
+    {
+      domain: "https://true-yak-33.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
