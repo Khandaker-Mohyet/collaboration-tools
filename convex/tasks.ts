07108c2e89c59b11279211c378e53d676cc461d6
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-08 14:44:04 +0600

    add and get task

---
+++ convex/tasks.ts
@@ -0,0 +1,21 @@
+import { v } from "convex/values";
+import {query,mutation} from "./_generated/server"
+
+export const getTasks = query({
+    args:{},
+    handler: async (ctx,args) =>{
+        const tasks = await ctx.db.query("tasks").collect()
+        return tasks
+    }
+});
+
+
+export const addTask = mutation({
+    args:{
+        text: v.string(),
+    },
+    handler: async (ctx,args)=>{
+    const taskId = await ctx.db.insert("tasks",{text:args.text,completed:false});
+    return taskId
+    }
+})
