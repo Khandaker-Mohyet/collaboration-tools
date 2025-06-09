61002414c4de2155511943e9c32f0c6499fedf1c
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-09 16:15:00 +0600

    users created

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,3 +1,12 @@
-import {defineSchema} from "convex/server";
+import {defineSchema, defineTable} from "convex/server";
+import { v } from "convex/values";
 
-export default defineSchema({})
+export default defineSchema({
+    users:defineTable({
+        name:v.optional(v.string()),
+        email:v.string(),
+        image:v.string(),
+        tokenIdentifier:v.string(),
+        isOnline:v.boolean()
+    })
+});
